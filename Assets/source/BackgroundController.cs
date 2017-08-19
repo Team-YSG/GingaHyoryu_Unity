c61e512fad76f44983e0a483e7ee052d02e4612c
MEGAYOKOYAN <y.y.as.fog@gmail.com>
2017-08-19 15:52:34 +0900

    first commit

---
+++ Assets/source/BackgroundController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour {
+
+	public float speed;
+	float size = 100;  //NOTE!
+
+	// Update is called once per frame
+	void Update()
+	{
+		transform.Translate(0, 0, speed);
+
+		if (this.transform.position.z + size < 0)
+		{
+			Debug.Log("out of display");
+			this.transform.Translate(0, 0, size * 2);
+		}
+	}
+}
